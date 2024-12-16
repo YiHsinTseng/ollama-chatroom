ea99a2571baf732589d238f25781f4563d7d6a90
easontseng <yhsints@gmail.com>
2024-12-17 00:39:36 +0800

    feat: verify Ollama status, restrict requests during initialization, and improve variable naming
    - [X] refactor(FE): unify stream and error message handling into a single system message function
    - [X] feat(FE): Notify user with a green dot when connected, red dot when disconnected or initializing
    - [X] feat(BE): Prevent requests from being sent while the model is initializing

--- server.js
+++ server.js
@@ -15,12 +15,36 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// TODO 預發API 確保 ollama 已成功載入程式（需要60s）並在前端提示
-
 // ollama stream data
-const OLLAMA_API_URL = `http://localhost:${OLLAMA_API_PORT}/api/chat`; // Replace with actual API URL
+const OLLAMA_API_CHAT_URL = `http://localhost:${OLLAMA_API_PORT}/api/chat`;
 
-async function streamOllamaData(socket, content) {
+let ollamaIsReady = false;
+
+async function verifyOllamaStatus(socket) {
+  const OLLAMA_API_CHECK_URL = `http://localhost:${OLLAMA_API_PORT}/api/generate`;
+  try {
+    const requestBody = {
+      model: 'kenneth85/llama-3-taiwan:latest',
+    };
+    const response = await axios.post(OLLAMA_API_CHECK_URL, requestBody);
+
+    if (response.data) {
+      // console.log(response.data);
+      ollamaIsReady = true;
+      socket.emit('ollamaIsReady', true);
+    } else {
+      ollamaIsReady = false;
+      socket.emit('ollamaIsReady', false);
+    }
+  } catch (error) {
+    // console.error('Error fetching data from Ollama API:', error);
+    ollamaIsReady = false;
+    socket.emit('ollamaIsReady', false);
+  }
+}
+
+// ollama server 根據api 中使用者請求生成回應
+async function streamOllamaResponse(socket, content) {
   try {
     const requestBody = {
       model: 'kenneth85/llama-3-taiwan:latest',
@@ -29,32 +53,51 @@ async function streamOllamaData(socket, content) {
         { role: 'user', content },
       ],
     };
-    const response = await axios.post(OLLAMA_API_URL, requestBody, {
+    const response = await axios.post(OLLAMA_API_CHAT_URL, requestBody, {
       responseType: 'stream',
     });
+
+    // 接收 ollama 回應
     response.data.on('data', (chunk) => {
       console.log('Received data chunk from Ollama API');
       socket.emit('ollama-stream', chunk.toString());
     });
   } catch (error) {
-    console.error('Error fetching data from Ollama API:', error);
-    socket.emit('ollama-stream', 'Error fetching data from Ollama API');
+    // console.error('Error fetching data from Ollama API:', error);
+    socket.emit('ollama-error', {
+      type: 'error',
+      message: 'Error fetching data from Ollama API',
+    });
   }
 }
 
 // socketio (without record)
 io.on('connection', (socket) => {
+  // 確認使用者連線
   console.log('A user connected');
 
+  // 確認LLM連線
+  verifyOllamaStatus(socket);
+
+  setInterval(() => {
+    verifyOllamaStatus(socket);
+  }, 60000);
+
+  // 接收 User 訊息並發送給 ollama LLM server
   socket.on('message', (msg) => {
     console.log(`Message from client: ${msg}`);
-    streamOllamaData(socket, msg);
+    if (ollamaIsReady === true) {
+      streamOllamaResponse(socket, msg);
+    } else {
+      socket.emit('ollama-error', { type: 'error', message: 'Error fetching data from Ollama API' });
+    }
     io.emit('message', msg);
   });
 
+  // 接收 ollama LLM server 生成的訊息給 User
   socket.on('request-stream', () => {
     console.log('Client requested Ollama data stream');
-    streamOllamaData(socket);
+    streamOllamaResponse(socket);
   });
 
   socket.on('disconnect', () => {
